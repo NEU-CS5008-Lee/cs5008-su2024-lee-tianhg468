cb6bfd7c8daad78fa27e3bd96b2b683dbf10ce19
Tian Huang <tianhuang@Tians-MacBook-Pro.local>
2024-05-17 11:09:26 -0700

    upload Lab2 assignment

--- lab-assignment2/calculator.c
+++ lab-assignment2/calculator.c
@@ -1,18 +1,51 @@
-//enter your name here
-//enter your email here
+//Tian Huang
+//huang.tian2@northeastern.edu
+
 #include<stdio.h>
 int main()
 {
-    int a,b,c, choice;
+    int a,b,c,remainder, choice;
     
     printf("Enter your choice\n");
     printf(" 1.addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n");
     scanf("%d",&choice);
     
-    printf("Enter a and b values");
+    printf("Enter a and b values: ");
     scanf("%d %d", &a, &b);
     
-    // Add your code here using switch
-           
+    switch (choice) {
+     case (1):
+      c = a + b;
+      printf("Addition\n");
+      printf("Sum = %d", c);
+      break;
+     case (2):
+      c = a - b;
+      printf("Subtraction\n");
+      printf("Difference = %d", c);
+      break;
+     case (3):
+      c = a * b;
+      printf("Multiplication\n");
+      printf("Product = %d", c);
+      break;
+     case (4):
+      if (b==0) {
+          printf("Divide by zero error");
+	  break;
+      }
+      else {
+          c = a / b;
+	  remainder = a % b;
+          printf("Division\n");
+          printf("Quotient = %d\n", c);
+	  printf("Remainder = %d", remainder);
+	  break;
+      }
+     default:
+      printf("Invalid option");
+      break;
+
+    }           
     return 0;
 }
--- lab-assignment2/employee.c
+++ lab-assignment2/employee.c
@@ -1,3 +1,6 @@
+/* Name: Tian Huang
+ * Email: huang.tian2@northeastern.edu*/
+
 /*C Program to read and print the n employee details using structure and dynamic memory allocation*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,16 +13,27 @@ typedef struct employee
     char Dept[10];
 
 }employee_t;
+
 /* Read the employee details using scanf*/
 void readData(int n, employee_t* s)
 {
-    // Add your code here
-}
+    for (int i=0; i<n; i++) {
+        printf("Enter the employeeID, Name, Designation and Department of Employee %d: \n", i+1);
+	scanf("%d %s %s %s",&(s->empId), s->Name, s->Designation, s->Dept);
+	s++;
+	printf("\n");
+    }
+}   
+
 /* Function to print the employee details*/
-void display(int n, employee_t * s)
+void display(int n, employee_t* s)
 {
-    // Add your code here
-    
+    for (int i=0; i<n; i++) {
+        printf("The details of Employee %d: \n empId: %d\n Name: %s\n Designation: %s\n Dept: %s\n", i+1, s->empId, 
+			s->Name, s->Designation, s->Dept);
+	s++;
+	printf("\n");
+    }    
 }
 
 /*----------Main Program--------*/
@@ -27,8 +41,12 @@ int main()
 
 {
     // Main Function to print the employee details
-    // Add your code here
-    
-
+    int n;
+    printf("Enter the number of employees: ");
+    scanf("%d", &n);
+    employee_t* s = (employee_t*) malloc(n * sizeof(employee_t));
+    readData(n, s);
+    display(n, s);
+    return 0;
 }
 
