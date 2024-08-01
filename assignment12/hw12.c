551914bb58dd1edab9d24928866427c61b22cb18
Tian Huang <tianhuang@Tians-MacBook-Pro.local>
2024-07-31 21:52:54 -0700

    Upload hw12

--- assignment12/hw12.c
+++ assignment12/hw12.c
@@ -1,5 +1,5 @@
-// name: <your name here>
-// email: <your email here>
+// name: Tian Huang
+// email: huang.tian2@northeastern.edu
 // Compile with:
 //
 // gcc -lpthread hw12.c -o hw12
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+
 
 #define NTHREADS 100
 
@@ -23,16 +24,22 @@ void* thread1 (void* vargp) {
 void* thread2 (void* vargp) {
   // add 5 to counter
   // *** YOUR CODE GOES HERE ***
+    counter = counter + 5;
+    return NULL;
 }
 
 void* thread3 (void* vargp) {
   // subtract 2 from counter
   // *** YOUR CODE GOES HERE ***
+    counter = counter - 2;
+    return NULL;
 }
 
 void* thread4 (void* vargp) {
   // subtract 10 from counter
   // *** YOUR CODE GOES HERE ***
+    counter = counter - 10;
+    return NULL;
 }
 
 int main() {
@@ -46,6 +53,9 @@ int main() {
   for (i=0; i < NTHREADS; ++i){
     pthread_create(&(tid[i]), NULL, thread1, NULL);
     // *** YOUR CODE GOES HERE ***
+      pthread_create(&(tid[NTHREADS + i]), NULL, thread2, NULL);
+      pthread_create(&(tid[NTHREADS * 2 + i]), NULL, thread3, NULL);
+      pthread_create(&(tid[NTHREADS * 3 + i]), NULL, thread4, NULL);
   }
 
   //wait until all threads are done
