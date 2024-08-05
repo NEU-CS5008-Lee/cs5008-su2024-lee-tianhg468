2450187d54c17b9d8fffdc77479d9b2df5e3169f
Tian Huang <tianhuang@Tians-MacBook-Pro.local>
2024-08-04 20:14:29 -0700

    Upload lab13

--- lab-assignment13/mp3-lab.c
+++ lab-assignment13/mp3-lab.c
@@ -1,20 +1,40 @@
-//enter your email here
-//enter your name here
+// huang.tian2@northeastern.edu
+// Tian Huang
 
 #include<stdio.h>
 #include<string.h>
 
 int compression(char arr[], int n, char res[]){
     
-    
-    
-    
     //insert your code here
-    
-    
-    
-    
-    
+    char *ptr=arr;
+    char *ptrend=arr+n;
+    char encoding[50];
+    int repeat;
+    strcpy(res,"");
+
+    while(ptr<ptrend)
+    {
+        char ch = *ptr;
+        repeat=0;
+        while(*ptr==ch)
+        {
+            ptr++;
+            repeat++;
+        }
+        if(repeat>1)
+        {
+            sprintf(encoding,"%c%d",ch,repeat);
+            strcat(res,encoding);
+        }else
+        {
+            sprintf(encoding,"%c",ch);
+            strcat(res,encoding);
+        }
+    }
+    printf("Compressed string is %s\n", res);
+    return strlen(res);
+
 }
  
 int main()
